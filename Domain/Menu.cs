c04cd7243d6166ca1f8fd41954e9ef6110e7b70c
Simon <olav@live.se>
2023-09-27 09:41:59 +0200

    Creating menu

---
+++ Domain/Menu.cs
@@ -0,0 +1,9 @@
+namespace ProductManager.Domain;
+
+public static class MenuManager
+{
+    public static void InitializeMenu()
+    {
+        Console.Write("Menu");
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ProductManager.Domain;
+
+namespace ProductManager;
+
+class Program
+{
+    static void Main()
+    {
+        MenuManager.InitializeMenu();
+    }
+}
